b773b8b0915f5e7f8f7420a757bc0e042feb78e9
Tuckers15 <taschulz@uvm.edu>
2025-04-29 12:36:53 -0400

    added tqdm to view progress with longer run times

--- benchmark.py
+++ benchmark.py
@@ -1,11 +1,12 @@
 import time
+from tqdm import tqdm
 import tracemalloc
 from shor import shor_classical_simulation
 from utils import generate_semiprime
 
-def benchmark_shor(min_bits=4, max_bits=12, trials_per_bit=5):
+def benchmark_shor(min_bits=4, max_bits=12, trials_per_bit=5): #default max bits should stay low just for testing purposes, 12 should run in ~5 seconds
     results = []
-    for bits in range(min_bits, max_bits + 1):
+    for bits in tqdm(range(min_bits, max_bits + 1)):
         for _ in range(trials_per_bit):
             N = generate_semiprime(bits)
             tracemalloc.start()
