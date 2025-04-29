7f8ca5589dca4d34f95939863110533e6ea57188
Tuckers15 <taschulz@uvm.edu>
2025-04-29 12:50:14 -0400

    30 bits and plots

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from plotter import plot_runtime, plot_memory, plot_success_rate
 from shor import shor_classical_simulation
 
 def main():
-    results = benchmark_shor(min_bits=4, max_bits=25, trials_per_bit=3)
+    results = benchmark_shor(min_bits=4, max_bits=30, trials_per_bit=3)
 
     plot_runtime(results)
     plot_memory(results)
     plot_success_rate(results)
 
-
+    
     print("\nRunning a verbose demo on N = 55 as an example")
     shor_classical_simulation(55, verbose=True)
 
